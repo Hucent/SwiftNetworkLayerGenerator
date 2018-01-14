7c31bf1dfc60ddddea3f9df73a18b8f22aeb3f95
Hucent <hucent@icloud.com>
2018-01-14 20:58:14 +0800

    ori version

---
+++ SwiftNetworkLayerGenerator/SwiftNetworkLayerGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftNetworkLayerGenerator
+//
+//  Created by Hucent on 2018/1/14.
+//  Copyright © 2018年 Hucent Studio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftNetworkLayerGenerator/SwiftNetworkLayerGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftNetworkLayerGenerator
+//
+//  Created by Hucent on 2018/1/14.
+//  Copyright © 2018年 Hucent Studio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
